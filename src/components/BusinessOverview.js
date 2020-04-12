a8abd6abf74945be59c45030c4e74183d2d4e33f
gisiona <gil-real@hotmail.com>
2020-04-12 01:16:51 -0300

    commit inicial

--- src/components/BusinessOverview.js
+++ src/components/BusinessOverview.js
@@ -9,7 +9,7 @@ class BusinessOverview extends Component {
         <p>
           O Vizi App é uma aplicação aberta que conecta você com o pequeno empresário com clientes e fornecedores. 
           Você dono de qualquer comercio pode fazer seu cadastro na plataforma online e começar a vender para todos os clientes de forma simples rápido 
-          e seguro, ou você também pode ser afiliado a um <b>VEZER</b> que faz o seu cadastro e divulga o seu negócio no bairro de atuação.         
+          e seguro, ou você também pode ser afiliado a um <b>VIZER</b> que faz o seu cadastro e divulga o seu negócio no bairro de atuação.         
         </p>
  
         <a className="btn btn-warning btn-lg" href="#">
