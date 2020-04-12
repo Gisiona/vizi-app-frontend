0742c84bea2b2cb6a1c38021247c7af08c71e236
gisiona <gil-real@hotmail.com>
2020-04-12 00:51:18 -0300

    commit inicial heroku

---
+++ build/precache-manifest.42114d017dbac486db8a2e8aa1aaaccb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a99852f3636018c1e986c9d5571846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff2955de7df90f6c63d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fff2955de7df90f6c63d",
+    "url": "/static/js/2.9b228640.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b228640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c21accec73d49d1fdd",
+    "url": "/static/js/main.c8b6009c.chunk.js"
+  },
+  {
+    "revision": "e931b65d6787e1862e47",
+    "url": "/static/js/runtime-main.b89cc0b2.js"
+  },
+  {
+    "revision": "0ebf80363a7dbb7c14baad4ddff71b19",
+    "url": "/static/media/comerciante.0ebf8036.png"
+  },
+  {
+    "revision": "bb4be1ba8ce965aaaa82c4d588b8b8f6",
+    "url": "/static/media/consumidor.bb4be1ba.png"
+  },
+  {
+    "revision": "b372b1bf441f9d39a4a3c5fcf04e7581",
+    "url": "/static/media/embaixador.b372b1bf.png"
+  }
+]);
